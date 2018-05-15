e1302ade87962559d03a3d664e5f949f7aa183ca
Samanvya <samanvya@gmail.com>
2018-05-15 23:08:11 +0800

    Update deploy_dockerhub.sh

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,4 @@
 !/bin/sh
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"
-else
-    TAG="$TRAVIS_BRANCH"
-fi
 docker build -f Dockerfile -t $DOCKER_USER/buzz:$TAG .
 docker push $DOCKER_USER/buzz:$TAG
